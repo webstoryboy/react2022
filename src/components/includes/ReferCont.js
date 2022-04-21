5afbeacd696c6f698a26b3ac7986d088197b7f40
webstoryboy <webstupids@gmail.com>
2022-04-21 16:48:32 +0900

    2022.04.21

--- src/components/includes/ReferCont.js
+++ src/components/includes/ReferCont.js
@@ -1,50 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import propType from "prop-types";
+import { Link } from "react-router-dom";
 
-function ReferItem({id, title, desc}){
+function ReferCont({id, title, desc, use, desc2, element, tag, version, view, image, link, Definition, Accessibility, Related, mdn, w3c}) {
   return (
-    <tr>
-      <td>{id}</td>
-      <td>{title}</td>
-      <td>
-        <Link to={{
-          pathname : "/refer-detail",
-          state: {id, title, desc},
-        }}>
-          {desc.slice(0, 180)}
-        </Link>
-      </td>
-    </tr>
+    <li>
+      <Link to={{
+        pathname: "refer-detail",
+        state: {id, title, desc, use, desc2, element, tag, version, view, image, link, Definition, Accessibility, Related, mdn, w3c}
+      }}>
+        <span className='num'>{id}</span>
+        <span className='title'>{title}</span>
+        <span className='desc'>{desc}</span>
+        <span className='use'>{use}</span>
+      </Link>
+    </li>
   )
 }
 
-function ReferCont({refers, color}) {
-  return (
-    <section className={`refer__cont ${color}`}>
-      <div className="container">
-        <div className="refer__inner">
-          <h2>CSS</h2>
-          <table>
-            <colgroup>
-              <col style={{width: "10%"}} />
-              <col style={{width: "20%"}} />
-              <col style={{width: "70%"}} />
-            </colgroup>
-            <tbody>
-              {refers.map(refer => (
-                <ReferItem 
-                  key={refer.id}
-                  id={refer.id}
-                  title={refer.title}
-                  desc={refer.desc}
-                />
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </section>  
-  )
+ReferCont.propType = {
+  id: propType.number.isRequired,
+  title: propType.string.isRequired,
+  desc: propType.string.isRequired,
+  use: propType.string.isRequired,
 }
 
-export default ReferCont
+export default ReferCont;
--- src/components/pages/Reference.js
+++ src/components/pages/Reference.js
@@ -4,24 +4,10 @@ import Contents from "../layout/Contents";
 import Footer from "../layout/Footer";
 import Title from "../layout/Title";
 import Contact from "../layout/Contact";
-import ReferCont from "../includes/ReferCont";
 import Loading from "../basics/Loading";
+import ReferCont from "../includes/ReferCont";
 import { gsap } from "gsap";
 import axios from "axios";
-
-// function Reference(){
-//     return (
-//         <>
-//             <Header color="light" />
-//             <Contents>
-//                 <Title title={["reference","book"]} color="light" />
-//                 <ReferCont color="light" />
-//                 <Contact />
-//             </Contents>
-//             <Footer color="light" />
-//         </>
-//     )
-// }
 
 class Reference extends React.Component {
     state = {
@@ -30,7 +16,7 @@ class Reference extends React.Component {
     }
 
     mainAnimation = () => {
-        setTimeout(() => {
+        setTimeout(() => { 
             gsap.to("#header", {
                 duration: 0.8, 
                 top: 0,
@@ -54,54 +40,84 @@ class Reference extends React.Component {
                 delay: 1.3,
                 ease: "power4.out"
             });
-            gsap.to(".refer__inner", {
-                duration: 0.7,
-                y: 0,
-                opacity: 1,
-                delay: 1.5,
-                ease: "power4.out"
-            });
+            // gsap.to(".refer__inner", {
+            //     duration: 0.5,
+            //     y: 0,
+            //     opacity: 1,
+            //     delay: 1.6,
+            //     ease: "power3.out"
+            // });
         }, 10)
     }
-    
-    getSite = async () => {
+
+    getRefers = async () => {
         const {
             data: {
                 data: {htmlRefer},
             },
         } = await axios.get("https://webstoryboy.github.io/react2022/src/assets/json/refer.json");
-        this.setState({refers: htmlRefer, isLoading: false})
+        this.setState({ refers: htmlRefer,  isLoading: false,})
         this.mainAnimation();
     }
 
     componentDidMount(){
         setTimeout(() => {
             document.getElementById("loading").classList.remove("loading__active");
-            document.querySelector("body").style.background = "#F0EEEB";
-            this.getSite();
+            this.getRefers();
         }, 2000);
     }
 
     render(){
         const {isLoading, refers} = this.state;
-
+        console.log(refers)
         return (
             <>
                 {isLoading ? (
-                    <Loading color="light" />
+                    <Loading color="black" />
                 ) : (
                     <>
-                        <Header color="light" />
+                        <Header />
                         <Contents>
-                            <Title title={["Reference","book"]} color="light" />
-                            <ReferCont refers={refers} color="light" />
+                            <Title title={["Portfolio","Site"]} />
+                            <section className="refer__cont">
+                                <div className="container">
+                                    <div className="refer__inner">
+                                        <h2>CSS</h2>
+                                        <ul className="refer__list">
+                                            {refers.map((refer) => (
+                                                <ReferCont 
+                                                    key={refer.id}
+                                                    id={refer.id}
+                                                    title={refer.title}
+                                                    desc={refer.desc}
+                                                    use={refer.use}
+
+                                                    desc2={refer.desc2}
+                                                    element={refer.element}
+                                                    tag={refer.tag}
+                                                    version={refer.version}
+                                                    view={refer.view}
+                                                    image={refer.image}
+                                                    link={refer.link}
+                                                    Definition={refer.Definition}
+                                                    Accessibility={refer.Accessibility}
+                                                    Related={refer.Related}
+                                                    mdn={refer.mdn}
+                                                    w3c={refer.w3c}
+                                                />
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </div>
+                            </section>
                             <Contact />
                         </Contents>
-                        <Footer color="light" />
+                        <Footer />
                     </>
                 )}
             </>
         )
+
     }
 }
 
